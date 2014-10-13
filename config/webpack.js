bf6ced5890314e5c5cae6662b678839f7615406f
Peter Marton <peter@secretsaucepartners.com>
2014-10-13 21:33:28 +0200

    Prepare project

---
+++ config/webpack.js
@@ -0,0 +1,83 @@
+/*!
+ * Facebook React Starter Kit | https://github.com/kriasoft/react-starter-kit
+ * Copyright (c) KriaSoft, LLC. All rights reserved. See LICENSE.txt
+ */
+
+/*
+ * Webpack configuration. For more information visit
+ * http://webpack.github.io/docs/configuration
+ * https://github.com/petehunt/webpack-howto
+ */
+
+'use strict';
+
+var webpack = require('webpack');
+
+module.exports = function(release) {
+  return {
+    output: {
+      path: './build/scripts/',
+      filename: 'app.js',
+      publicPatch: './build/scripts/'
+    },
+
+    cache: !release,
+    debug: !release,
+    devtool: false,
+    entry: './src/app/app.js',
+
+    stats: {
+      colors: true,
+      reasons: !release
+    },
+
+    plugins: release ? [
+      new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin()
+    ] : [],
+
+    resolve: {
+      extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx']
+    },
+
+    module: {
+      preLoaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'jshint'
+        }
+      ],
+
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: 'style!css'
+        },
+        {
+          test: /\.less$/,
+          loader: 'style!css!less'
+        },
+        {
+          test: /\.gif/,
+          loader: 'url-loader?limit=10000&mimetype=image/gif'
+        },
+        {
+          test: /\.jpg/,
+          loader: 'url-loader?limit=10000&mimetype=image/jpg'
+        },
+        {
+          test: /\.png/,
+          loader: 'url-loader?limit=10000&mimetype=image/png'
+        },
+        {
+          test: /\.(js|jsx)$/,
+          loader: 'jsx-loader?harmony'
+        }
+      ]
+    }
+  };
+};
---
+++ gulpfile.js
@@ -0,0 +1,130 @@
+'use strict';
+
+var path = require('path');
+
+var gulp = require('gulp');
+var webpack = require('webpack');
+
+// Load plugins
+var $ = require('gulp-load-plugins')();
+var mainBowerFiles = require('main-bower-files');
+
+var WATCH = true;
+var RELEASE = false;
+var DEST = './build/';
+
+// Bundle
+gulp.task('bundle', function (cb) {
+  var started = false;
+  var config = require('./config/webpack.js')(RELEASE);
+  var bundler = webpack(config);
+
+  function bundle(err, stats) {
+    if (err) {
+      throw new $.util.PluginError('webpack', err);
+    }
+
+    $.util.log('[webpack]', stats.toString({colors: true}));
+
+    if (!started) {
+      started = true;
+      return cb();
+    }
+  }
+
+  if (WATCH) {
+    bundler.watch(200, bundle);
+  } else {
+    bundler.run(bundle);
+  }
+});
+
+// HTML
+gulp.task('html', function () {
+  return gulp.src('src/views/*.html')
+    .pipe($.useref())
+    .pipe(gulp.dest(DEST))
+    .pipe($.size());
+});
+
+// Images
+gulp.task('images', function () {
+  return gulp.src('src/images/**/*')
+    .pipe(gulp.dest(path.join(DEST, 'images')))
+    .pipe($.size());
+});
+
+// Fonts
+gulp.task('fonts', function () {
+  return gulp.src(mainBowerFiles())
+    .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+    .pipe($.flatten())
+    .pipe(gulp.dest(path.join(DEST, 'fonts')))
+    .pipe($.size());
+});
+
+// Clean
+gulp.task('clean', function () {
+  return gulp.src([
+      path.join(DEST, 'styles'),
+      path.join(DEST, 'images'),
+      path.join(DEST, 'views'),
+      path.join(DEST, 'scripts')
+    ],
+    {read: false}).pipe($.clean());
+});
+
+
+// HTML Bundle
+gulp.task('htmlBundle', ['bower'], function () {
+  var assets = $.useref.assets();
+
+  return gulp.src('./src/views/*.html')
+    .pipe(assets)
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.csso()))
+    .pipe(assets.restore())
+    .pipe($.useref())
+    .pipe(gulp.dest(DEST));
+});
+
+// Build
+gulp.task('build', ['clean', 'html', 'htmlBundle', 'bundle', 'images', 'fonts']);
+
+// Default task
+gulp.task('default', ['clean'], function () {
+  RELEASE = true;
+  WATCH = false;
+
+  gulp.start('build');
+});
+
+// Bower helper
+gulp.task('bower', function () {
+  gulp.src('src/bower_components/**/*.js', {base: 'src/bower_components'})
+    .pipe(gulp.dest(path.join(DEST, 'bower_components')));
+});
+
+// Watch
+gulp.task('serve', ['build'], function () {
+  $.livereload.listen();
+
+  WATCH = true;
+
+  // Watch bower
+  gulp.watch('bower.json', ['fonts'])
+    .on('change', $.livereload.changed);
+
+  // Watch .html files
+  gulp.watch('src/views/*.html', ['html', 'htmlBundle'])
+    .on('change', $.livereload.changed);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images'])
+    .on('change', $.livereload.changed);
+
+  $.connect.server({
+    root: ['build', 'tmp'],
+    port: 8000
+  });
+});
