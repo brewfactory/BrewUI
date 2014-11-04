f2d3653a03a2dff1e093c3ca52f2c175c4441553
Peter Marton <peter@risingstack.com>
2014-11-04 07:46:47 +0100

    Add react and actions interfaces

--- index.js
+++ index.js
@@ -80,8 +80,13 @@ function build(distPath, force) {
 function extendToIsomorphic () {
   debug('Generate Isomorphic interfaces: .app, .client');
 
-  exports.app = require('./src/scripts/app/app');
+  exports.App = require('./src/scripts/app/app');
   exports.client = require('./src/scripts/client/client');
+
+  exports.React = require('react');
+  exports.actions = {
+    navigateAction: require('flux-router-component').navigateAction
+  };
 }
 
 
